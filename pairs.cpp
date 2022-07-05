140627b104a9fd193a1db192b8c7096879cd4f3a
Gaganyadav2209 <98753163+Gaganyadav2209@users.noreply.github.com>
2022-07-05 09:36:14 +0530

    Update pairs.cpp

--- pairs.cpp
+++ pairs.cpp
@@ -1,1 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;    
+
+int main() {
+   
+pair<int,string> p;
+/*
+p = make_pair (2,"abcd");
+                          // two ways of adding value to pair 
+// p = {1,"gagan"};
+pair<int,string> p1 = p;  // here copy of p is stored in pair p1 
+// but if we use address then same value are copied and upon changing it can e changed
+p1.first = 3;
+
+cout<<p.first<<" "<<p.second<<endl;
+pair<int,int> p_array[3];  // two arrays can be made a pair in case there is some relationship b/w the arrays
+p_array[0]={1,2};
+p_array[1]={2,3};
+p_array[2]={3,4};
+swap(p_array[0],p_array[2]);
+for(int i=0;i<3;i++){
+    cout<<p_array[i].first<<" "<<p_array[i].second<<endl;
+}
+
+*/
+cin>>p.first;
+cin>>p.second;
+cout<<p.first<<endl;
+cout<<p.second<<endl;
+   
+   
+}
 
